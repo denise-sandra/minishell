dbf67e8b87a70e94365fcf09087197cff9a7e2bf
Sandra Kanna <skanna@k0r3p14.42mulhouse.fr>
2024-05-09 16:25:52 +0200

    prompt done

--- src/main.c
+++ src/main.c
@@ -6,14 +6,29 @@
 /*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:03:16 by skanna            #+#    #+#             */
-/*   Updated: 2024/05/08 19:02:06 by skanna           ###   ########.fr       */
+/*   Updated: 2024/05/09 16:24:10 by skanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+// #include "minishell.h"
+#include "../includes/minishell.h"
 
 int	main(int argc, char **argv, char **envp)
 {
-     init_env(envp)
-     init_prompt(); //-->usar readline en vez de GNL!
+	char	*input;
+
+	//init_prompt(); //-->usar readline en vez de GNL
+	while (1)
+	{
+		input = readline("minishell$ ");
+		if (!input)
+			break ;
+		if (*input == '\0')
+			printf("No input provided\n");
+		else
+			add_history(input);
+		free(input);
+		// init_env(envp);
+	}
+	return (0);
 }
