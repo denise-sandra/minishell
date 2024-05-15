322908168691ee1bc1a27ae09a040fcdcdb26568
Sandra Kanna <skanna@k0r4p13.42mulhouse.fr>
2024-05-15 20:37:36 +0200

    agregue una condicion de entrada para solo ejecutar token con contenido, antes una linea de comando solo con espacios daba invalid read

--- src/exec/execution.c
+++ src/exec/execution.c
@@ -6,18 +6,22 @@
 /*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:03:22 by skanna            #+#    #+#             */
-/*   Updated: 2024/05/14 20:16:06 by skanna           ###   ########.fr       */
+/*   Updated: 2024/05/15 20:35:33 by skanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void execution(t_minishell *minishell)
+
+void	execution(t_minishell *minishell)
 {
-     if (minishell->token[0]->type == TOKEN_SPECIAL_COMMAND)
-          execute_special_command(minishell, minishell->token[0]->value);
-     else if (minishell->token[0]->type == TOKEN_COMMAND)
-          printf("not so special command\n");
-     else
-          printf("first token is not a command\n");
+	if (minishell->token && minishell->token[0])
+	{
+		if (minishell->token[0]->type == TOKEN_SPECIAL_COMMAND)
+			execute_special_command(minishell, minishell->token[0]->value);
+		else if (minishell->token[0]->type == TOKEN_COMMAND)
+			printf("not so special command\n");
+		else
+			printf("first token is not a command\n");
+	}
 }
