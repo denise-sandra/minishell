531dabcfbdba4e04dd7583bc88985e389d9d9dc5
Denise Erjavec <deniseerjavec@Air-de-Denise.lan>
2024-06-07 13:37:29 +0200

    error syntax corregido

--- src/parser/check_syntax_errors.c
+++ src/parser/check_syntax_errors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_syntax_errors.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: derjavec <derjavec@student.42.fr>          +#+  +:+       +#+        */
+/*   By: deniseerjavec <deniseerjavec@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:03:59 by skanna            #+#    #+#             */
-/*   Updated: 2024/06/06 17:57:14 by derjavec         ###   ########.fr       */
+/*   Updated: 2024/06/07 13:36:53 by deniseerjav      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,10 +55,10 @@ static int error_append(t_minishell *mini)
 	t_pretok	*list;
 
      list = mini->pretok;
-	if (list && list->next && list->type == APPEND && list->next->type == APPEND)
+	if (list && list->next && list->type == OUT && list->next->type == OUT)
 	{
-		if (list->next->next == NULL && list->next->next->c == '-'\
-			&&  list->next->next->c == '>')
+		if (list->next->next == NULL || (list->next->next && (list->next->next->c == '-'\
+			||  list->next->next->c == '>')))
 			return (1);
 		if (list->next->next->c == 32 && list->next->next->next == NULL)
 			return (1);
@@ -71,11 +71,11 @@ static int error_heredoc(t_minishell *mini)
 	t_pretok	*list;
 
      list = mini->pretok;
-	if (list && list->next && list->type == HEREDOC && list->next->type == HEREDOC)
+	if (list && list->next && list->type == IN && list->next->type == IN)
 	{
-		if (list->next->next == NULL && list->next->next->c == '*'\
-			&& list->next->next->c == '-' && list->next->next->c == '/'\
-			&&  list->next->next->c == '<')
+		if (list->next->next == NULL || (list->next->next && (list->next->next->c == '*'\
+			|| list->next->next->c == '-' || list->next->next->c == '/'\
+			|| list->next->next->c == '<')))
 			return (1);
 		if (list->next->next->c == 32 && list->next->next->next == NULL)
 			return (1);
