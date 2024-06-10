e0223c013f4ed00277aace838d6a1ade5c58ce56
Sandra Kanna <skanna@k0r1p11.42mulhouse.fr>
2024-06-10 18:45:54 +0200

    manejo de espacios dentro de variables OK + valgrind OK

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,18 +6,21 @@
 /*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:03:59 by skanna            #+#    #+#             */
-/*   Updated: 2024/06/10 15:41:01 by skanna           ###   ########.fr       */
+/*   Updated: 2024/06/10 18:44:10 by skanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	expand_var(t_mini *mini, t_pretok **cur, t_token **list)
+static void	expand_var(t_mini *mini, t_pretok **cur, t_token **list, t_type q)
 {
 	char	*env_name;
 	char	*env_value;
+	char	**temp;
+	int		i;
 
 	*cur = (*cur)->next;
+	i = 0;
 	if (!(*cur) || (*cur)->type != ENV)
 	{
 		if (tok_list("$", TEXT, list) != 0)
@@ -36,8 +39,23 @@ static void	expand_var(t_mini *mini, t_pretok **cur, t_token **list)
 	free(env_name);
 	if (!env_value)
 		return (ft_error("Memory allocation error", mini));
-	if (tok_list(env_value, ENV, list) != 0)
-		return (free(env_value), ft_error("Memory allocation error", mini));
+	if (q == D_Q)
+	{
+		if (tok_list(env_value, ENV, list) != 0)
+			return (free(env_value), ft_error("Memory allocation error", mini));
+	}
+	else
+	{
+		temp = ft_split(env_value, ' ');
+		if (!temp)
+			return (ft_error("Memory allocation error", mini));
+		while (temp[i])
+		{
+			if (tok_list(temp[i++], TEXT, list) != 0)
+				return (free_tab(temp), ft_error("Memory allocation error", mini));
+		}
+		free_tab(temp);
+	}
 	free(env_value);
 }
 
@@ -50,7 +68,7 @@ static void	last_quotes(t_mini *mini, t_pretok **cur, t_token **lst, t_type q)
 	while (*cur && (*cur)->type != q)
 	{
 		if ((*cur)->type == EXP)
-			expand_var(mini, cur, lst);
+			expand_var(mini, cur, lst, q);
 		else
 		{
 			str = ft_strjoin_char(str, (*cur)->c);
@@ -94,6 +112,40 @@ static void	pipes_and_opts(t_mini *mini, t_pretok **cur, t_token **list)
 	*cur = (*cur)->next;
 }
 
+// static char	*expand_var_in_str(t_mini *mini, t_pretok **cur, char *str)
+// {
+// 	char	*env_name;
+// 	char	*env_value;
+// 	char	*temp;
+
+// 	*cur = (*cur)->next;
+// 	if (!(*cur) || (*cur)->type != ENV)
+// 	{
+// 		str = ft_strjoin_char(str, '$');
+// 		if (!str)
+// 			return (ft_error("Memory allocation error", mini), NULL);
+// 		return (str);
+// 	}
+// 	//env_name = NULL;
+// 	while (*cur && (*cur)->type == ENV)
+// 	{
+// 		env_name = ft_strjoin_char(env_name, (*cur)->c);
+// 		if (!env_name)
+// 			return (ft_error("Memory allocation error", mini), str);
+// 		*cur = (*cur)->next;
+// 	}
+// 	env_value = get_env_value(mini->env, env_name);
+// 	free(env_name);
+// 	if (!env_value)
+// 		return (ft_error("Memory allocation error", mini), str);
+// 	temp = ft_strjoin(str, env_value);
+// 	free(env_value);
+// 	if (!temp)
+// 		return (ft_error("Memory allocation error", mini), str);
+// 	free(str);
+// 	return (temp);
+// }
+
 static void	chars_and_empty(t_mini *mini, t_pretok **cur, t_token **list)
 {
 	char	*join;
@@ -107,6 +159,12 @@ static void	chars_and_empty(t_mini *mini, t_pretok **cur, t_token **list)
 			if (!join)
 				return (ft_error("Memory allocation error", mini));
 		}
+		// else if ((*cur)->type == EXP)
+		// {
+		// 	join = expand_var_in_str(mini, cur, join);
+		// 	if (!join)
+		// 		return (ft_error("Memory allocation error", mini));
+		// }
 		else if ((*cur)->type == EMPTY)
 		{
 			if (tok_list("", EMPTY, list) != 0)
@@ -136,7 +194,7 @@ void parser(t_mini *mini)
 		else if (cur_pretok->type == IN || cur_pretok->type == OUT)
 			parse_redirs(mini, &cur_pretok, &(mini->token));
 		else if (cur_pretok->type == EXP)
-			expand_var(mini, &cur_pretok, &(mini->token));
+			expand_var(mini, &cur_pretok, &(mini->token), EMPTY);
 		else if (cur_pretok->type == D_Q || cur_pretok->type == S_Q)
 			last_quotes(mini, &cur_pretok, &(mini->token), cur_pretok->type);
 		else
@@ -145,15 +203,8 @@ void parser(t_mini *mini)
 			return ;
 	}
 	clean_pretokens(mini);
+	//parse_commands(mini); //para despues, tener en cuenta que esto puede hacer un return despues de un error
 	t_token *print = mini->token;
-	printf ("tok list: %p \n", mini->token);
-	while (print)
-	{
-		printf("token: %s  type: %i\n", print->value, print->type);
-		print = print->next;
-	}
-	parse_commands(mini); //para despues, tener en cuenta que esto puede hacer un return despues de un error
-	print = mini->token;
 	while (print)
 	{
 		printf("new: %s  type: %i\n", print->value, print->type);
