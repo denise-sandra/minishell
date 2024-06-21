77623780c956830cc8e8e110f65f76f698f0c279
Denise Erjavec <deniseerjavec@Air-de-Denise.lan>
2024-06-21 13:14:16 +0200

    ejecuta scripts, si no hay interprete ejecuta con sh, hay que achicar la funcion y hacer mas tests por las dudas, tmb chequer los mensajes de error

--- src/exec/cmd_exec.c
+++ src/exec/cmd_exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cmd_exec.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sandra <sandra@student.42.fr>              +#+  +:+       +#+        */
+/*   By: deniseerjavec <deniseerjavec@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:03:22 by skanna            #+#    #+#             */
-/*   Updated: 2024/06/20 15:13:16 by sandra           ###   ########.fr       */
+/*   Updated: 2024/06/21 13:11:29 by deniseerjav      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,11 +66,70 @@ static int	cmd_exec_utils(t_mini *mini, t_token *tmp, char **paths)
 	return (0);
 }
 
+static int	exec_script(t_mini *mini, t_token *tmp)
+{
+	int	script;
+	char *name;
+	char *line;
+	char *args[3];
+	
+	name = NULL;
+	if (ft_strncmp(tmp->cmd_tab[0], "./", 2) == 0)
+		name = tmp->cmd_tab[0] + 2;
+	else if (ft_strncmp(tmp->cmd_tab[0], "sh", ft_strlen(tmp->cmd_tab[0])) == 0 ||\
+	ft_strncmp(tmp->cmd_tab[0], "bash", ft_strlen(tmp->cmd_tab[0])) == 0)
+		name = tmp->cmd_tab[1];
+	if (name)
+	{
+		script = open(name, O_RDONLY);
+		if (script == -1)
+		{
+			close (script);
+			return (ft_error(mini, NULL, strerror(errno)), 1);
+		}
+		args[0] = NULL;
+		while (1)
+		{
+			line = get_next_line(script);
+			if (!line)
+				break;
+			else if (strncmp(line, "#!", 2) == 0)
+			{
+				args[0] = ft_strdup(line + 2);
+				free (line);
+				if (!args[0])
+					return (close(script), ft_error(mini, NULL, strerror(errno)), 1);
+				args[0][ft_strlen(args[0]) - 1] = '\0';
+				break ;
+			}
+			else if (strncmp(line, "#", 1) != 0)
+			{
+				args[0] = ft_strdup("/bin/sh");
+				break ;
+			}
+			free(line);	
+		}
+		if (!args[0])
+			return (close(script), 1);
+		args[1] = name;
+		args[2] = NULL;
+		if (execve(args[0], args, mini->env_char) == -1) 
+		{
+            ft_error(mini, "", strerror(errno));
+            return (free(args[0]), close(script), 1);
+        }
+		return (free(args[0]), close(script), ft_error(mini, "not found", NULL), 1);
+	}
+	return (0);
+}
+
 void	cmd_exec(t_mini *mini, t_token *tmp)
 {
 	char	**paths;
 	int		exec_ret;
 
+	if (exec_script(mini, tmp) == 1)
+		return ;
 	paths = pars_path(mini);
 	if (paths == NULL)
 		return (ft_error(mini, "Failed to parse path", NULL));
