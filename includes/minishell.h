78dab04f40c23be7b3b075af0b8426c0189d24f5
Sandra Kanna <skanna@k0r1p11.42mulhouse.fr>
2024-06-24 12:25:16 +0200

    inicializacion de args dentro de script

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: deniseerjavec <deniseerjavec@student.42    +#+  +:+       +#+        */
+/*   By: skanna <skanna@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 17:03:55 by skanna            #+#    #+#             */
-/*   Updated: 2024/06/21 23:54:14 by deniseerjav      ###   ########.fr       */
+/*   Updated: 2024/06/24 11:15:54 by skanna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,8 +88,8 @@ int			init_fds(t_mini *mini);
 int			ft_dup(t_mini *mini, int i);
 int			ft_dup_tubes(t_mini *mini, int i);
 int			fill_fd(t_mini *mini);
-void	    parse_and_execute(t_mini *mini, char *input);
-int	        exec_script(t_mini *mini, t_token *tmp);
+void		parse_and_execute(t_mini *mini, char *input);
+int			exec_script(t_mini *mini, t_token *tmp);
 
 //builtin fucntions
 void		echo_command(t_token *cur);
